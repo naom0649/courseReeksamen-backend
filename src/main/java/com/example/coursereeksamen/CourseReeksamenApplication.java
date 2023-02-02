2ac221490d48a29d4db41223b26bbd0b26d2bb5b
naom0649 <naom0649@stud.kea.dk>
2023-02-02 09:34:09 +0100

    Initial commit

---
+++ src/main/java/com/example/coursereeksamen/CourseReeksamenApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursereeksamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseReeksamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseReeksamenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coursereeksamen/CourseReeksamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursereeksamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseReeksamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
